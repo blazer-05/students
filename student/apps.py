5b4247e71231429e01d61f7abb78c410e2778e23
Blazer <blazer-05@mail.ru>
2017-12-15 10:19:09 +0300

    my commit

--- student/apps.py
+++ student/apps.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
@@ -5,3 +6,10 @@ from django.apps import AppConfig
 
 class StudentConfig(AppConfig):
     name = 'student'
+
+class StudentsAppConfig(AppConfig):
+    name = 'student'
+    verbose_name = u'База Студентов'
+
+    def ready(self):
+        from student import signals
